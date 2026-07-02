9dbc7a38c04cf518325f98055c1745a7e9b50b02
akram-hany-front-end <akramhanyahmed@gmail.com>
2026-07-02 05:24:53 +0300

    api created successfully

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,20 +1,19 @@
-"use client"
-import { createContext, useState } from "react"
+"use client";
 
-export const ThemeContext = createContext()
+import { createContext, useState } from "react";
+
+export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-    const [mode, setMode] = useState{ "dark"}
-    const toggle = () => {
-        setMode((prev) => prev === "dark" ? "light" : "dark")
-    }
-    return (
-        <ThemeContext.Provider value={{ toggle, mode }}>
-            <div className={`theme ${mode}`}>
-                {children}
-            </div>
+  const [mode, setMode] = useState("dark");
 
-        </ThemeContext.Provider>
-    )
+  const toggle = () => {
+    setMode((prev) => (prev === "dark" ? "light" : "dark"));
+  };
 
-}
+  return (
+    <ThemeContext.Provider value={{ toggle, mode }}>
+      <div className={`theme ${mode}`}>{children}</div>
+    </ThemeContext.Provider>
+  );
+};
